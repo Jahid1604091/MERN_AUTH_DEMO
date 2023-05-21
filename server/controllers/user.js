897b3dd0cdeb8d5fdf40f8f1cfa5c3cbf6140e63
Jahid1604091 <jh409780@gmail.com>
2023-05-21 08:48:23 +0600

    basic route and controller

---
+++ server/controllers/user.js
@@ -0,0 +1,5 @@
+
+//public
+export const auth_user = (req,res)=>{
+    res.status(200).json({message:'Auth user',success:true})
+}
--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.js';
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+app.use('/api/users',userRoutes);
 
 app.get('/',(req,res)=>res.send('Server Running'));
 
