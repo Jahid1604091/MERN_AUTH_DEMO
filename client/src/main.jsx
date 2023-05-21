440d439eddc67868e76def041315a19b4c55955a
Jahid1604091 <jh409780@gmail.com>
2023-05-21 18:12:11 +0600

    redux toolkit setup

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -11,18 +11,23 @@ import {
 import Home from "./pages/Home.jsx";
 import Login from "./pages/Login.jsx";
 import Register from "./pages/Register.jsx";
+import { Provider } from "react-redux";
+import { store } from "./store.js";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-  <Route path="/" element={<App />}>
-    <Route index path="/" element={<Home/>} />
-    <Route index path="/login" element={<Login/>} />
-    <Route index path="/register" element={<Register/>} />
-  </Route>)
+    <Route path="/" element={<App />}>
+      <Route index path="/" element={<Home />} />
+      <Route index path="/login" element={<Login />} />
+      <Route index path="/register" element={<Register />} />
+    </Route>
+  )
 );
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </Provider>
 );
