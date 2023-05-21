eef3374b2073cb21704c3ae0e7e8dbbf9ba8e74b
Jahid1604091 <jh409780@gmail.com>
2023-05-21 09:38:15 +0600

    DB setup

--- server/index.js
+++ server/index.js
@@ -2,9 +2,10 @@ import express from 'express';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.js';
 import { errHandler, notFound } from './middleware/errorHandler.js';
+import { connectDB } from './config/db.js';
 dotenv.config();
 const PORT = process.env.PORT || 5000;
-
+connectDB();
 const app = express();
 
 app.use('/api/users',userRoutes);
